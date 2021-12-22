f5614622d28c32db0c96354f8e9310e9b8af7c7e
Aaron <iibargurenca20dw@ikzubirimanteo.com>
2021-12-22 12:04:22 +0100

    Añadiendo roles por defecto a la aplicación

--- database/migrations/2021_12_20_114333_create_roles_table.php
+++ database/migrations/2021_12_20_114333_create_roles_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateRolesTable extends Migration
@@ -19,6 +20,14 @@ class CreateRolesTable extends Migration
             $table->string('description');
             $table->timestamps();
         });
+        $data = [[ 'id' => '1',
+        'name' => 'usuario',
+        'description' => 'Usuario que puede unirse a cursos'],[ 'id' => '2',
+        'name' => 'creador',
+        'description' => 'Creador que puede unirse a cursos y poder crearlos'],[  'id' => '3',
+        'name' => 'administrador',
+        'description' => 'Administrador que gestiona el sitio web']];
+        DB::table('roles')->insert($data);
     }
 
     /**
