471770a28b9e8150ea83adf78f5c8efd4f0eb7e5
Unai Corzo <ucorzoga20dw@ikzubirimanteo.com>
2022-01-19 11:41:27 +0100

    Validación formulario crear curso

--- public/js/create.js
+++ public/js/create.js
@@ -108,4 +108,137 @@ $(document).ready(function () {
         map.addObject(marker);
     });
     // FIN API MAPA
+
+    // VALIDACIÓN CREAR CURSO
+    $.validator.addMethod("priceMin", function (value, element) {
+        if (value < 0) {
+            return this.optional(element);
+        }
+        return true;
+    });
+
+    $.validator.addMethod("categoriesMin", function (value, element) {
+        if (value == "") {
+            return false;
+        }
+        return true;
+    });
+
+    $.validator.addMethod("categoriesMax", function (value, element) {
+        if (value.split(";").length > 6) {
+            return false;
+        }
+        return true;
+    });
+
+    $.validator.addMethod("imageFormat", function (value, element) {
+        let filename = value.split(".");
+        if (filename.length != 2) {
+            return this.optional(element);
+        }
+        let extension = filename[1];
+        if (extension == "jpg" || extension == "jpeg" || extension == "png") {
+            return true;
+        }
+        return this.optional(element);
+    });
+
+    $.validator.addMethod("locationSelection", function (value, element) {
+        if (value == "" && $("#map").css("opacity") == 1) {
+            return false;
+        }
+        return true;
+    });
+
+    let idioma = $('html').attr('lang');
+
+    let title_1 = "El título es requerido";
+    let title_2 = "El título no puede superar los 64 caracteres";
+    let price_1 = "El precio es requerido";
+    let price_2 = "El precio no puede ser negativo";
+    let categories_1 = "Mínimo de categorías: 1";
+    let categories_2 = "Máximo de categorías: 5";
+    let image_1 = "La imagen es requerida";
+    let image_2 = "Formatos válidos: .jpg, .jpeg, .png";
+    let description_1 = "La descripción es requerida";
+    let location_1 = "La ubicación es requerida";
+
+    if (idioma == 'en') {
+        title_1 = "Title required";
+        title_2 = "Title cannot exceed 64 characters";
+        price_1 = "Price required";
+        price_2 = "Price cannot be negative";
+        categories_1 = "Minimum categories: 1";
+        categories_2 = "Maximum categories: 5";
+        image_1 = "Image required";
+        image_2 = "Valid formats: .jpg, .jpeg, .png";
+        description_1 = "Description required";
+        location_1 = "Location required";
+    }
+    else if (idioma == 'eu') {
+        title_1 = "Izenburua beharrezkoa da";
+        title_2 = "Izenburuak ezin ditu 64 karaktere baino gehiago izan";
+        price_1 = "Prezioa beharrezkoa da";
+        price_2 = "Prezioa ezin da negatiboa izan";
+        categories_1 = "Gutxieneko kategoriak: 1";
+        categories_2 = "Gehieneko kategoriak: 5";
+        image_1 = "Irudia beharrezkoa da";
+        image_2 = "Baliozko formatuak: .jpg, .jpeg, .png";
+        description_1 = "Deskribapena beharrezkoa da";
+        location_1 = "Kokapena beharrezkoa da";
+    }
+
+    $("#crear_curso").validate({
+        ignore: "",
+        onkeyup: false,
+        rules: {
+            title: {
+                required: true,
+                maxlength: 64,
+            },
+            price: {
+                required: true,
+                priceMin: true,
+            },
+            categories: {
+                categoriesMin: true,
+                categoriesMax: true,
+            },
+            image: {
+                required: true,
+                imageFormat: true,
+            },
+            description: {
+                required: true,
+            },
+            location: {
+                locationSelection: true,
+            },
+        },
+        messages: {
+            title: {
+                required: title_1,
+                maxlength: title_2,
+            },
+            price: {
+                required: price_1,
+                priceMin: price_2,
+            },
+            categories: {
+                categoriesMin: categories_1,
+                categoriesMax: categories_2,
+            },
+            image: {
+                required: image_1,
+                imageFormat: image_2,
+            },
+            description: {
+                required: description_1,
+            },
+            location: {
+                locationSelection: location_1,
+            },
+        },
+    });
+    // FIN VALIDACIÓN CREAR CURSO
 });
--- resources/views/create_course.blade.php
+++ resources/views/create_course.blade.php
@@ -13,13 +13,13 @@
             </div>
             <div class="row mx-4 mx-sm-0 mx-md-0 mx-lg-0">
                 <div class="col-12 mb-5 d-flex justify-content-between align-items-center">
-                    <form action="{{ route('course-store', app()->getLocale()) }}" method="post" class="row" enctype="multipart/form-data">
+                    <form action="{{ route('course-store', app()->getLocale()) }}" method="post" class="row align-content-start" id="crear_curso" enctype="multipart/form-data">
                         {{ csrf_field() }}
 
                         <div class="col-6 form-group px-5 mb-2 d-flex justify-content-between align-items-start" id="nombre">
                             <i class="fas fa-sign mb-2 mt-2 iconos_sesion"></i>
                             <div class="campos_registro">
-                                <input class="form-control p-2 campos_sesion" type="text" name="name" placeholder="{{ __('Título curso') }}"/>
+                                <input class="form-control p-2 campos_sesion" type="text" id="title" name="title" placeholder="{{ __('Título curso') }}"/>
                             </div>
                         </div>
                         <div class="col-6 form-group px-5 mb-2 d-flex justify-content-between align-items-start">
@@ -34,16 +34,16 @@
                         <div class="col-6 form-group px-5 mb-2 d-flex justify-content-between align-items-start" id="nombre">
                             <i class="fas fa-hand-holding-usd mb-2 mt-2 iconos_sesion"></i>
                             <div class="campos_registro">
-                                <input class="form-control p-2 campos_sesion" type="number" step="0.01" name="price" placeholder="{{ __('Precio curso') }}"/>
+                                <input class="form-control p-2 campos_sesion" type="number" id="price" step="0.01" name="price" placeholder="{{ __('Precio curso') }}"/>
                             </div>
                         </div>
                         <div class="col-6 form-group px-5 mb-2 d-flex justify-content-between align-items-start" id="nombre">
                             <i class="fas fa-sign mb-2 mt-2 iconos_sesion"></i>
-                            <div class="campos_registro">
-                                <textarea class="form-control p-2 campos_sesion" rows="5" name="description" placeholder="{{ __('Descripción curso') }}" style="resize: none;"></textarea>
+                            <div class="campos_registro_area">
+                                <textarea class="form-control p-2 campos_sesion" rows="5" id="description" name="description" placeholder="{{ __('Descripción curso') }}" style="resize: none;"></textarea>
                             </div>
                         </div>
-                        <div class="col-6 form-group px-5 mb-2 mt-5 d-flex flex-column justify-content-start align-items-start gap-3" id="nombre">
+                        <div class="col-6 form-group px-5 mb-2 d-flex flex-column justify-content-start align-items-start gap-3" id="nombre">
                             <p class="m-0">{{ __('Selecciona las categorías:') }}</p>
                             <div class="selector_cat mt-2">
                                 @foreach ($categories as $category)
@@ -59,18 +59,22 @@
                                     }
                                 </script>
                             </div>
-                            <input type="hidden" name="categories" id="categories" value="">
+                            <div class="campos_registro">
+                                <input type="hidden" name="categories" id="categories" value="">
+                            </div>
                         </div>
-                        <div class="col-6 form-group px-5 mb-2 mt-5 d-flex justify-content-between align-items-center flex-wrap gap-3">
+                        <div class="col-6 form-group px-5 mb-2 d-flex justify-content-between align-items-center flex-wrap gap-3">
                             <i class="fas fa-street-view iconos_sesion"></i>
                             <div class="form-check form-switch items campos_sesion">
                                 <input class="form-check-input presencial" type="checkbox" id="switchPresencial" name="presencial" value="no">
                                 <label class="form-check-label" for="switchPresencial">{{ __('Presencial') }}</label>
                             </div>
                             <div class="map mt-2" id="map" style="width: 100%; height: 300px;"></div>
-                            <input type="hidden" value="" name="location" id="location">
+                            <div class="campos_registro">
+                                <input type="hidden" value="" name="location" id="location">
+                            </div>
                         </div>
-                        <div class="col-12 d-flex justify-content-center align-items-center text-center mt-3">
+                        <div class="col-12 d-flex justify-content-center align-items-center text-center">
                             <button class="btn submit_sesion boton_sesion py-2 items text-uppercase"
                                 id="submitButton" type="submit">{{ __('Crear') }}
                             </button>
