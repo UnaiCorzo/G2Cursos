623c84c5f707c72a0be408527efb8082de397680
Ekaitz10 <74232631+agarciago20dw@users.noreply.github.com>
2022-01-24 12:32:25 +0100

    Mejorar vistas de confirmar registro y recuperar contraseña (responsive)

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -1,1 +1,29 @@
-<h1>Se te ha enviado un correo, confírmalo</h1>
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>{{ __('Recuperar contraseña') }} | G2Cursos</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.ico') }}" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="seccion_recuperar">
+        <h2 class="section-heading m-0 text-center text-dark h3 text-uppercase mb-3 enviado_email">{{ __('Se te ha enviado un correo, confírmalo') }}</h2>
+        
+    </div>
+
+    <!-- LINKS SCRIPTS DE BOOTSTRAP -->
+    <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('js/recovery.js') }}"></script>
+</body>
+</html>
