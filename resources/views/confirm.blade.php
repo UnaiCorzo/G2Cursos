0966fb58efda19927d545a3d44004a3531c4b593
Ekaitz10 <74232631+agarciago20dw@users.noreply.github.com>
2022-01-24 12:38:38 +0100

    Title en vistas recuperar contraseña y verificar email

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>{{ __('Recuperar contraseña') }} | G2Cursos</title>
+    <title>{{ __('Verificar email') }} | G2Cursos</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.ico') }}" />
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
