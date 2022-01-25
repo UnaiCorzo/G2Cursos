4d22c98c35a75708d79be1ffe87bc993641b019a
Unai Corzo <ucorzoga20dw@ikzubirimanteo.com>
2022-01-25 08:30:13 +0100

    Vista confirmación en tres idiomas

--- resources/views/confirm.blade.php
+++ resources/views/confirm.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<html>
     
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>{{ __('Verificar email') }} | G2Cursos</title>
+    <title>G2Cursos</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.ico') }}" />
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
@@ -14,9 +14,11 @@
 </head>
 
 <body>
-    <div class="seccion_recuperar">
-        <h2 class="section-heading m-0 text-center text-dark h3 text-uppercase mb-3 enviado_email">{{ __('Se te ha enviado un correo, confírmalo') }}</h2>
-        
+    <div class="seccion_recuperar pb-5">
+        <span><i class="fa-5x bi bi-envelope-check"></i></span>
+        <div class="section-heading m-0 text-center text-dark h6 mb-3 enviado_email">Se te ha enviado un correo, confírmalo</div>
+        <div class="section-heading m-0 text-center text-dark h6 mb-3 enviado_email">An email has been sent to you, please confirm it</div>
+        <div class="section-heading m-0 text-center text-dark h6 mb-3 enviado_email">Mezu bat bidali dizugu, baieztatu</div>
     </div>
 
     <!-- LINKS SCRIPTS DE BOOTSTRAP -->
@@ -25,5 +27,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('js/recovery.js') }}"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 </body>
 </html>
