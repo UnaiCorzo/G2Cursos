cde845219f49cf00dba221fb9057f3f5e1d4bacd
Ekaitz10 <iibargurenca20dw@ikzubirimanteo.com>
2022-01-28 12:34:41 +0100

    Mejora de la api de cursos y categorías

--- routes/api.php
+++ routes/api.php
@@ -20,13 +20,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/course', function(){
-    return Course::all();
+    return Course::all('name','location','description');
 });
 Route::get('/course/{course}', function(Course $course){
     return response()->json($course,201);
 });
 Route::get('/category', function(){
-    return Category::all();
+    return Category::all('name','color');
 });
 Route::get('/category/{category}', function(Category $category){
     return response()->json($category,201);
